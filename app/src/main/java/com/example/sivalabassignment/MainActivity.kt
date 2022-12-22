af9ed725650fa4c16d3cebc7c652b94851b96de6
Siva <sivadotpc@gmail.com>
2022-12-22 20:27:10 +0530

    Lab2Screen and Navigation commit

--- app/src/main/java/com/example/sivalabassignment/MainActivity.kt
+++ app/src/main/java/com/example/sivalabassignment/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.sivalabassignment
 
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.example.sivalabassignment.ui.theme.*
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.ramcosta.composedestinations.DestinationsNavHost
 
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,6 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
                     DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
